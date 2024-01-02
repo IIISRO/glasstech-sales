177f1dc41cb44fe79e57edd09558a6a6a2cefe07
Ilgar Shukuroff <ilgarshukuroff@Ilgars-MacBook-Pro.local>
2024-01-02 16:40:39 +0100

    deploy  conf

--- glasstechsales/settings.py
+++ glasstechsales/settings.py
@@ -216,5 +216,5 @@ EMAIL_BACKEND = 'glasstechsales.email_backend.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'ilgarshukuroff@gmail.com'
-EMAIL_HOST_PASSWORD = 'cknbwyiverniqyyx'
+EMAIL_HOST_USER = 'glasstechsales01@gmail.com'
+EMAIL_HOST_PASSWORD = 'uhpimqrsnyobsjmz'
